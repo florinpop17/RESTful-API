1419268d20181cc7f2a206c5643346738690f104
Florin Pop <popflorin1705@yahoo.com>
2017-01-27 14:29:31 +0200

    add genre model

--- models/genre.js
+++ models/genre.js
@@ -18,3 +18,8 @@ var Genre = module.exports = mongoose.model('Genre', genreSchema);
 module.exports.getGenres = function(callback, limit) {
     Genre.find(callback).limit(limit);
 }
+
+// Add Genre
+module.exports.addGenre = function(genre, callback) {
+    Genre.create(genre, callback);
+}
