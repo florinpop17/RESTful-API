caa38107e37d658924e3bcb3b35863d996401253
Florin Pop <popflorin1705@yahoo.com>
2017-01-27 12:59:22 +0200

    connect to mongoose

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/bookstore');
+var db = mongoose.connection();
+
