6c4d2112ecfcb042e9dfc01c68694f1cb3296686
Florin Pop <popflorin1705@yahoo.com>
2017-01-27 14:50:34 +0200

    update book

--- models/genre.js
+++ models/genre.js
@@ -23,3 +23,12 @@ module.exports.getGenres = function(callback, limit) {
 module.exports.addGenre = function(genre, callback) {
     Genre.create(genre, callback);
 }
+
+// Update Genre
+module.exports.updateGenre = function(id, genre, options, callback) {
+    var query = {_id: id};
+    var update = {
+        name: genre.name
+    }
+    Genre.findOneAndUpdate(query, update, options, callback);
+}
