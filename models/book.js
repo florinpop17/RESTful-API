5bf9641304e9e48d05ebff47b9910fab408bfaac
Florin Pop <popflorin1705@yahoo.com>
2017-01-27 14:55:05 +0200

    update book model

--- models/book.js
+++ models/book.js
@@ -51,3 +51,19 @@ module.exports.getBookById = function(id, callback) {
 module.exports.addBook = function(book, callback) {
     Book.create(book, callback);
 }
+
+// Update Books
+module.exports.updateBook = function(id, book, options, callback) {
+    var query = {_id: id};
+    var update = {
+        title: book.title,
+        genre: book. genre,
+        description: book.description,
+        author: book.author,
+        publisher: book.publisher,
+        pages: book.pages,
+        image_url: book.image_url,
+        buy_url: book.buy_url
+    }
+    Book.findOneAndUpdate(query, update, options, callback);
+}
