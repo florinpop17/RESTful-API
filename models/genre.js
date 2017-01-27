ce921213a085f7509356be580162ab282d5eeb5f
Florin Pop <popflorin1705@yahoo.com>
2017-01-27 13:39:16 +0200

    fixed genre code... it was in book.js

---
+++ models/genre.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+// Genre Schema
+var genreSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Genre = module.exports = mongoose.model('Genre', genreSchema);
+
+// Get Genres
+module.exports.getGenres = function(callback, limit) {
+    Genre.find(callback).limit(limit);
+}
