d858881f516bdde4438af9ff17736128db529343
Florin Pop <popflorin1705@yahoo.com>
2017-01-27 13:44:09 +0200

    book model Schema

--- models/book.js
+++ models/book.js
@@ -6,29 +6,6 @@ var bookSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    genre: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    author: {
-        type: String,
-        required: true
-    },
-    publisher: {
-        type: String
-    },
-    pages: {
-        type: String
-    },
-    image_url: {
-        type: String  
-    },
-    buy_url: {
-        type: String
-    },
     create_date: {
         type: Date,
         default: Date.now
