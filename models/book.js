aeb14c7e8a30e934ccf2717430fb5e270622c014
Florin Pop <popflorin1705@yahoo.com>
2017-01-27 13:43:55 +0200

    book model

---
+++ models/book.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+
+// Book Schema
+var bookSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    publisher: {
+        type: String
+    },
+    pages: {
+        type: String
+    },
+    image_url: {
+        type: String  
+    },
+    buy_url: {
+        type: String
+    },
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Book = module.exports = mongoose.model('Book', genreSchema);
+
+// Get Genres
+module.exports.getGenres = function(callback, limit) {
+    Genre.find(callback).limit(limit);
+}
